25df8ba2976d7c2cc2a0c9cae170801efebb3295
Linda Kohn <linda.kohn@umu.se>
2023-11-01 13:18:31 +0100

    edits to check colors blue or red

--- python_analysis/bin/xgboost_classifier.py
+++ python_analysis/bin/xgboost_classifier.py
@@ -27,7 +27,6 @@ def clin_info(meta_file, clin_var):
 def make_predictions(infile, clin_var_info):
     ''' Random forest classifier with 1000 trees, train/test divided 80/20 '''
     indata = pd.DataFrame(pd.read_csv(infile, sep=',', header=0))
-    print(indata)
     indata = indata.T
     indata.columns = indata.iloc[0]
     indata = indata.drop(indata.index[0])
@@ -43,13 +42,13 @@ def make_predictions(infile, clin_var_info):
     # Transform labels to numbers
     labelencoder = LabelEncoder()
     labels = labelencoder.fit_transform(indata.iloc[:, -1])
-   
     pred_data = indata.drop(indata.columns[-1], axis=1).drop(indata.columns[0], axis=1)
     data_train, data_test, labels_train, labels_test = train_test_split(pred_data, labels, test_size=0.2, random_state=44)
     data_train = data_train.astype('float')
     data_test = data_test.astype('float')
     data_for_mean = data_test.copy()
     data_for_mean['clin_var_info'] = labels_test
+    data_for_mean.to_csv('data_for_mean_MSI_'  + date.today().strftime('%y%m%d') + '.csv')
     means_df = data_for_mean.astype(float).groupby('clin_var_info').mean()
     feat_colors = []
     for _ , values in means_df.items():
@@ -79,7 +78,7 @@ def make_predictions(infile, clin_var_info):
     roc_auc = roc_auc_score(labels_test, pred_prob)
     feat_importance = pd.DataFrame(model.feature_importances_)
     feat_importance.index = pred_data.columns
-    fpr, tpr, thresholds = roc_curve(labels_test, pred_prob, pos_label=1)
+    fpr, tpr, thresholds = roc_curve(labels_test, pred_prob, pos_label=1)    
     return fpr, tpr, roc_auc, feat_importance, feat_colors
 
     
@@ -88,7 +87,7 @@ def plot_importances(feat_importance, out_prefix, clin_var, feat_colors):
     ''' Plot feature importances of top 20 most informative ASVs '''
     feat_importance['col'] = feat_colors
     feat_out = feat_importance.sort_values(by=0, ascending=False)
-    feat_out.to_csv(str(out_prefix) + '_feat_importance_' + clin_var + '_' + date.today().strftime('%y%m%d') + '.csv')
+    #feat_out.to_csv(str(out_prefix) + '_feat_importance_' + clin_var + '_' + date.today().strftime('%y%m%d') + '.csv')
     top = feat_importance.sort_values(by=0, ascending=True).tail(20)
     top.reset_index(inplace=True)
     #names = top['index'].str.split('_').str[3:].apply('_'.join).str.split('.').str[0]
@@ -99,7 +98,7 @@ def plot_importances(feat_importance, out_prefix, clin_var, feat_colors):
     plt.xlabel('Feature importance')
     plt.ylabel(None)
     plt.subplots_adjust(left=0.6)
-    plt.savefig(str(out_prefix) + '_feat_importances_' + clin_var + '_' + date.today().strftime('%y%m%d') + '.png', dpi=200)
+    #plt.savefig(str(out_prefix) + '_feat_importances_' + clin_var + '_' + date.today().strftime('%y%m%d') + '.png', dpi=200)
     plt.show()
     plt.close()
       
@@ -128,27 +127,27 @@ def main():
     plot_importances(cancer_feat_importances, out_prefix, 'Cancer', cancer_colors)
     plot_roc(cancer_fpr, cancer_tpr, cancer_auc, out_prefix, 'Cancer')
     
-    #MSI vs MSS
+    # #MSI vs MSS
     msi_info = clin_info(meta_file, 'MSI')
     msi_fpr, msi_tpr, msi_auc, msi_feat_importances, msi_colors = make_predictions(infile, msi_info)
     plot_importances(msi_feat_importances, out_prefix, 'MSI', msi_colors)
     plot_roc(msi_fpr, msi_tpr, msi_auc, out_prefix, 'MSI') 
        
-    # Cancer specific death
-    death_info = clin_info(meta_file, 'Can_spec_death')
-    death_fpr, death_tpr, death_auc, death_feat_importances, death_colors = make_predictions(infile, death_info)
-    plot_importances(death_feat_importances, out_prefix, 'Can_spec_death', death_colors)
-    plot_roc(death_fpr, death_tpr, death_auc, out_prefix, 'Can_spec_death')   
-    # KRAS
-    kras_info = clin_info(meta_file, 'KRAS')
-    kras_fpr, kras_tpr, kras_auc, kras_feat_importances, kras_colors = make_predictions(infile, kras_info)
-    plot_importances(kras_feat_importances, out_prefix, 'KRAS', kras_colors)
-    plot_roc(kras_fpr, kras_tpr, kras_auc, out_prefix, 'KRAS')    
-    # BRAF
-    braf_info = clin_info(meta_file, 'BRAF')
-    braf_fpr, braf_tpr, braf_auc, braf_feat_importances, braf_colors = make_predictions(infile, braf_info)
-    plot_importances(braf_feat_importances, out_prefix, 'BRAF', braf_colors)
-    plot_roc(braf_fpr, braf_tpr, braf_auc, out_prefix, 'BRAF')
+    # # Cancer specific death
+    # death_info = clin_info(meta_file, 'Can_spec_death')
+    # death_fpr, death_tpr, death_auc, death_feat_importances, death_colors = make_predictions(infile, death_info)
+    # plot_importances(death_feat_importances, out_prefix, 'Can_spec_death', death_colors)
+    # plot_roc(death_fpr, death_tpr, death_auc, out_prefix, 'Can_spec_death')   
+    # # KRAS
+    # kras_info = clin_info(meta_file, 'KRAS')
+    # kras_fpr, kras_tpr, kras_auc, kras_feat_importances, kras_colors = make_predictions(infile, kras_info)
+    # plot_importances(kras_feat_importances, out_prefix, 'KRAS', kras_colors)
+    # plot_roc(kras_fpr, kras_tpr, kras_auc, out_prefix, 'KRAS')    
+    # # BRAF
+    # braf_info = clin_info(meta_file, 'BRAF')
+    # braf_fpr, braf_tpr, braf_auc, braf_feat_importances, braf_colors = make_predictions(infile, braf_info)
+    # plot_importances(braf_feat_importances, out_prefix, 'BRAF', braf_colors)
+    # plot_roc(braf_fpr, braf_tpr, braf_auc, out_prefix, 'BRAF')
 
 
 if __name__ == "__main__":
